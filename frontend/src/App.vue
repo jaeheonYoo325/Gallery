99a399f1edfd0e0b6a82f3ba9091aed9192d2453
jaeheonYoo325 <yoojh0325@gmail.com>
2025-03-09 11:06:52 +0900

    Vue Router 추가.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
-import Home from '@/pages/Home.vue';
 </script>
 
 <template>
 	<Header></Header>
-	<Home></Home>
+	<RouterView></RouterView>
 	<Footer></Footer>
 </template>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/pages/Home.vue';
+import Login from '@/pages/Login.vue';
 
-createApp(App).mount('#app');
+const routes = [
+	{ path: '/', component: Home },
+	{ path: '/login', component: Login },
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
+
+createApp(App).use(router).mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
