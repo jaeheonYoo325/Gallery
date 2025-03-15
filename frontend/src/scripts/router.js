8a3aa55cedef31218f12e77bf92e1bf8fe7d0fe2
jaeheonYoo325 <yoojh0325@gmail.com>
2025-03-15 15:26:18 +0900

    장바구니 기능 구현3.

--- frontend/src/scripts/router.js
+++ frontend/src/scripts/router.js
@@ -1,12 +1,14 @@
 import Cart from '@/pages/Cart.vue';
 import Home from '@/pages/Home.vue';
 import Login from '@/pages/Login.vue';
+import Order from '@/pages/Order.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
 	{ path: '/', component: Home },
 	{ path: '/login', component: Login },
 	{ path: '/cart', component: Cart },
+	{ path: '/order', component: Order },
 ];
 
 const router = createRouter({
